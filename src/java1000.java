a7e8b292b69bd658cb43a1c0811402380fa99da6
beombeom1119 <qjarl1119@naver.com>
2022-02-16 15:10:46 +0900

    Initial commit

---
+++ src/java1000.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class java1000 {
+
+    public static void main(String[] args) {
+        Scanner bum = new Scanner(System.in);
+        int a =bum.nextInt();
+        int b =bum.nextInt();
+
+        System.out.println(a+b);
+
+
+    }
+}
